bb411a4de97edf0c6f1410d448c6cd9a75e3a16f
Momeedo <44546259+Momeedo@users.noreply.github.com>
2021-01-06 13:51:51 +0100

    First Commit

---
+++ TechnicalTest/Assets/Scripts/Classes/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Message
+{
+    public string sender;
+    public string text;
+
+    public Message(string sender, string text)
+    {
+        this.sender = sender;
+        this.text = text;
+    }
+}
